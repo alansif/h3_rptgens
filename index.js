15274a40aeff64ac789a1e5875519a8542b984db
alansif <alansif@gmail.com>
2019-11-26 11:05:23 +0800

    fix

--- index.js
+++ index.js
@@ -73,8 +73,7 @@ app.get("/api/query", function(req, res){
                 {pattern: `**/${pid}*.jpg`, path: config.matpath[3]},
                 {pattern: [`**/${pid}*.jpg`,`**/${pid}*.pdf`], path: config.matpath[4]},
             ];
-            let files = patterns.map(mp => globby.sync(mp.pattern, {cwd:mp.path}).sort().reverse());
-            console.log(files);
+            let files = patterns.map(mp => globby.sync(mp.pattern, {cwd:mp.path, caseSensitiveMatch: false}).sort().reverse());
             res.status(200).json({info:result.recordset, files});
         } catch(err) {
             res.status(500).json(err);
